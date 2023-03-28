3cd7b13df14388d60dd14186a7b5438ac57915a4
herryho <herry.heyi@gmail.com>
2023-03-28 13:21:07 +0800

    add table BifrostLinkedAccountRegistered

--- src/mappings/CrossInOutHandlers.ts
+++ src/mappings/CrossInOutHandlers.ts
@@ -1,6 +1,10 @@
 import { SubstrateEvent } from "@subql/types";
 import { Balance, AccountId } from "@polkadot/types/interfaces";
-import { BifrostCrossedIn, BifrostCrossedOut } from "../types";
+import {
+  BifrostCrossedIn,
+  BifrostCrossedOut,
+  BifrostLinkedAccountRegistered,
+} from "../types";
 import { FIL_CURRENCY } from "./constants";
 
 // Handing talbe【CrossInOut】, Event【CrossedIn】
@@ -76,6 +80,41 @@ export async function handleCrossInOutCrossedOut(
     record.timestamp = Math.floor(event.block.timestamp.getTime() / 1000);
 
     record.filecoin_multisig_cid = null;
+
+    await record.save();
+  }
+}
+
+// Handing module【CrossInOut】, Event【LinkedAccountRegistered】
+export async function handleCrossInOutLinkedAccountRegistered(
+  event: SubstrateEvent
+): Promise<void> {
+  logger.info(`${event}`);
+  let evt = JSON.parse(JSON.stringify(event));
+  const blockNumber = event.block.block.header.number.toNumber();
+  //   Create the record by constructing id from blockNumber + eventIndex
+  const record = new BifrostLinkedAccountRegistered(
+    `${blockNumber.toString()}-${event.idx.toString()}`
+  );
+
+  const {
+    event: {
+      data: [currencyId, address, location],
+    },
+  } = evt;
+
+  if (JSON.stringify(currencyId) == JSON.stringify(FIL_CURRENCY)) {
+    record.account_id = (address as AccountId).toString();
+    record.block_height = blockNumber;
+
+    // Store currencyId as a JSON string.
+    record.currency_id = JSON.stringify(currencyId);
+    record.location = JSON.stringify(location);
+
+    // Transfer Date type to Unix timestamp type.
+    record.timestamp = Math.floor(event.block.timestamp.getTime() / 1000);
+
+    record.fee_airdrop_tx_hash = null;
 
     await record.save();
   }
